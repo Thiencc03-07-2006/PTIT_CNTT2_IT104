5f44e7e200f19b04e33cd7aebacb985892cc7f1a
Thiencc03-07-2006 <cct20062250.@gmail.com>
2025-08-06 18:19:21 +0700

    Bai 3

--- PTIT_CNTT2_IT104_Session03/src/PTIT_CNTT2_IT104_Session03_Bai02.ts
+++ PTIT_CNTT2_IT104_Session03/src/PTIT_CNTT2_IT104_Session03_Bai02.ts
@@ -1,2 +1,3 @@
 let username="Alice";
 // username=123
+//Bao loi vi khi khai bao ta dan username="Alice" nen typeScript da ngam hieu username la kieu string len khi dan cho no 123 kieu number thi se bao loi
